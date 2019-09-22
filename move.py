2dbefe64990e5f0e97fdc183e07f2febd5105225
Matthias Endler <matthias-endler@gmx.net>
2019-09-22 22:16:59 +0200

    First version

---
+++ move.py
@@ -0,0 +1,33 @@
+import time
+
+boxes = 100*[None]
+BATCH_SIZE = 100
+
+PACK_TIME = 0.02
+TRANSPORT_TIME = 0.5
+
+def pack(boxes):
+  print("Packing...")
+  time.sleep(BATCH_SIZE * PACK_TIME)
+  batch, boxes[:] = boxes[:BATCH_SIZE], boxes[BATCH_SIZE:]
+  return batch
+
+def transport(batch):
+  print("Transporting...")
+  time.sleep(TRANSPORT_TIME)
+  
+def unpack(batch):
+  print("Unpacking...")
+  time.sleep(BATCH_SIZE * PACK_TIME)
+
+def batches(items, size):
+  for i in range(0, len(items), size):
+    yield items[i:i+size]
+
+def main():
+  for batch in batches(boxes, 10):
+    batch = pack(boxes)
+    transport(batch)
+    unpack(batch)
+
+main()
---
+++ move_async.py
@@ -0,0 +1,34 @@
+import asyncio
+
+boxes = 100*[None]
+BATCH_SIZE = 100
+
+PACK_TIME = 0.02
+TRANSPORT_TIME = 0.5
+
+async def pack(boxes):
+  print("Packing...")
+  await asyncio.sleep(PACK_TIME)
+
+async def transport(batch):
+  print("Transporting...")
+  await asyncio.sleep(TRANSPORT_TIME)
+  
+async def unpack(batch):
+  print("Unpacking...")
+  await asyncio.sleep(PACK_TIME)
+
+def batches(items, size):
+  for i in range(0, len(items), size):
+    yield items[i:i+size]
+
+async def move(batch):
+    await pack(batch)
+    await transport(batch)
+    await unpack(batch)
+
+async def main():
+  for batch in batches(boxes, 10):
+    await asyncio.gather(move(batch))
+
+asyncio.run(main())
